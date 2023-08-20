e011e767a6d644ef229cdadf1207a9292e2d1bb1
Dominykas Kisonas <dominykas.kisonas@citadele.lv>
2023-08-20 21:08:37 +0300

    MWPFE-6 - installed shadcn-ui and radix ui

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,17 +1,21 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import { Theme } from "@radix-ui/themes";
+import "./globals.css";
+import "@radix-ui/themes/styles.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'MagicJourney Labs',
-  description: 'team website',
-}
+  title: "MagicJourney Labs",
+  description: "team website",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <Theme>
+        <body className={inter.className}>{children}</body>
+      </Theme>
     </html>
-  )
+  );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,27 @@
+import { Button } from "@/components/ui/button";
+import { EnvelopeOpenIcon } from "@radix-ui/react-icons";
+import { Card, Flex, Avatar, Box, Text } from "@radix-ui/themes";
+
 export default function Home() {
   return (
-    <div>MagicJourney Labs</div>
-  )
+    <div>
+      <div>MagicJourney Labs</div>
+      <Button>
+        <EnvelopeOpenIcon className="mr-2 h-4 w-4" /> Mygtukas
+      </Button>
+      <Card>
+        <Flex gap="3" align="center">
+          <Avatar size="3" src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop" radius="full" fallback="T" />
+          <Box>
+            <Text as="div" size="2" weight="bold">
+              Teodros Girmay
+            </Text>
+            <Text as="div" size="2" color="gray">
+              Engineering
+            </Text>
+          </Box>
+        </Flex>
+      </Card>
+    </div>
+  );
 }
