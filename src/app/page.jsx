19b817766cb86647761606b2361b41c5ca2e9487
Marius Ratnikas <marius.ratnikas@gmail.com>
2023-08-21 00:31:00 +0300

    add hygryph cms to Home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,46 @@
+'use client'
+
+import React from 'react';
+import { GraphQLClient, gql } from 'graphql-request';
+
+const url = 'https://api-eu-west-2.hygraph.com/v2/clljx627l1rex01uf1rdedh2g/master';
+const graphConnect = new GraphQLClient(url);
+
+const query = gql`
+  query Heroes {
+    heroes {
+      createdAt
+      heroText
+      id
+      publishedAt
+      updatedAt
+    }
+  }
+`;
+
 export default function Home() {
+  const [heroes, setHeroes] = React.useState([]);
+
+  React.useEffect(() => {
+    async function fetchHeroes() {
+      try {
+        const data = await graphConnect.request(query);
+        setHeroes(data.heroes);
+      } catch (error) {
+        console.error('Error fetching heroes:', error);
+      }
+    }
+
+    fetchHeroes();
+  }, []);
+
   return (
-    <div>MagicJourney Labs</div>
-  )
+    <div>
+      {heroes.length > 0 ? (
+        <div>{heroes[0].heroText}</div>
+      ) : (
+        <div>No heroes available</div>
+      )}
+    </div>
+  );
 }
