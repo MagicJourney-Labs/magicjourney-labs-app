cd62859b155a6a45e4b1620a7b45ed51dafd678a
Marius Ratnikas <marius.ratnikas@gmail.com>
2023-08-21 18:54:51 +0300

    fix style

--- src/components/ui/Post.jsx
+++ src/components/ui/Post.jsx
@@ -1,11 +1,11 @@
-const Post = ({data: post}) => {
-  return (
-    <>
-    <div>
-          <h2>{post.title}</h2>
-          <p>{post.content.text}</p>
-        </div>
-    </>
-  )
+const Post = ({ data: post }) => {
+    return (
+        <>
+            <div>
+                <h2>{post.title}</h2>
+                <p>{post.content.text}</p>
+            </div>
+        </>
+    )
 }
 export default Post
