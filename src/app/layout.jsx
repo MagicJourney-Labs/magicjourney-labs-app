3ed2fb7feedf87d41b4fe02d31d332747961b51e
Laura Matiukievic <laura.matiukievic@gmail.com>
2023-08-21 11:51:50 +0300

    MWPFE-18 - added footer

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,17 +1,22 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Footer from "@/components/ui/footer";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'MagicJourney Labs',
-  description: 'team website',
-}
+  title: "MagicJourney Labs",
+  description: "team website",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
