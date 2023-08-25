add7b2d531d11b8695443089a7995e5892ee029a
Dominykas Kisonas <dominykas.kisonas@citadele.lv>
2023-08-25 17:43:50 +0300

    Testing without force-dynamic in page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ import FeaturedPosts from "@/components/ui/FeaturedPosts";
 import { fetchGraphQL } from "@/lib/graphql-utils";
 import Link from "next/link";
 
-export const dynamic = "force-dynamic";
+// export const dynamic = "force-dynamic";
 
 async function getProjects() {
   const query = `
