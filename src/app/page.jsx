6fbb90bc42c70d6fcbb7773549d7e1417eea1e70
Dominykas Kisonas <dominykas.kisonas@citadele.lv>
2023-08-25 16:37:39 +0300

    Added force-dynamic segment config to home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,8 @@ import FeaturedPosts from "@/components/ui/FeaturedPosts";
 import { fetchGraphQL } from "@/lib/graphql-utils";
 import Link from "next/link";
 
+export const dynamic = "force-dynamic";
+
 async function getProjects() {
   const query = `
         query Heroes {
