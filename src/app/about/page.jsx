1501339fae584e1ec9f2029fab2a937ca00d35e4
Marius Ratnikas <marius.ratnikas@gmail.com>
2023-08-29 00:21:34 +0300

    about page did

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,11 +1,36 @@
 import Link from "next/link";
 import React from "react";
+import { fetchGraphQL } from '@/lib/graphql-utils';
 
-const About = () => {
+async function getAbout() {
+  const query = `
+  query About {
+    abouts {
+      content {
+        text
+      }
+      title
+      id
+    }
+  }
+  
+  `;
+
+  const data = await fetchGraphQL(query);
+  return data;
+}
+
+const About = async () => {
+  const { abouts } = await getAbout();
   return (
     <>
-      <div>About</div>
-      <Link href="/">Home</Link>
+    <Link href="/">Home</Link>
+      {abouts.map(about => (
+        <div key={(about.id)}>
+        <div>{about.title}</div>
+        <p>{about.content.text}</p>
+        </div>
+      ))}
     </>
   );
 };
