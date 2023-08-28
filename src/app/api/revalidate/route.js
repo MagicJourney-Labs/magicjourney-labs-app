f280a03b16af4c2df12dfbf3e0281fb10d1e012a
Martynas-J <Aurimhas@gmail.com>
2023-08-29 01:50:20 +0300

    add Blog page v1.9

--- src/app/api/revalidate/route.js
+++ src/app/api/revalidate/route.js
@@ -1,21 +1,43 @@
 import { NextResponse } from "next/server";
 
 import { revalidateTag } from "next/cache";
-import ApiUtils from "@/lib/api-utils";
 
 export const revalidate = true;
 
 export const POST = async (request) => {
-  const requestBody = await request.json();
-
-  if (ApiUtils.isSecretValid(requestBody, process.env.HYGRAPH_WEBHOOK_SECRET_REVALIDATE)) {
-    return NextResponse.json({ message: "Invalid secret" }, { status: 401 });
-  }
-
   const tag = request.nextUrl.searchParams.get("tag");
-
   if (!tag) {
     return NextResponse.json({ message: "Missing tag param" }, { status: 400 });
+  }
+
+  const secret = process.env.HYGRAPH_POSTS_WEBHOOK_KEY
+
+  const signature = headers().get("gcms-signature");
+
+  const [rawSign, rawEnv, rawTimestamp] = signature.split(', ');
+
+  const sign = rawSign.replace('sign=', '');
+  const EnvironmentName = rawEnv.replace('env=', '');
+  const Timestamp = parseInt(rawTimestamp.replace('t=', ''));
+
+  const bodyReq = await req.json()
+  const body = JSON.stringify(bodyReq)
+
+  let payload = JSON.stringify({
+      Body: body,
+      EnvironmentName,
+      TimeStamp: Timestamp,
+  });
+
+  const hash = createHmac('sha256', secret).update(payload).digest('base64');
+  const isValid = sign === hash;
+
+  if (req.method !== 'POST') {
+      return res.status(405).json({ message: 'Method Not Allowed' });
+  }
+
+  if (!isValid) {
+      return new NextResponse(" Invalid token Error :(", { status: 401 });
   }
 
   try {
