ba545791ce2c6f6734c0fc2682eeed276cf9b2a9
Laura Matiukievic <laura.matiukievic@gmail.com>
2023-08-28 23:05:34 +0300

    e

--- src/components/ui/Post.jsx
+++ src/components/ui/Post.jsx
@@ -1,11 +1,11 @@
 const Post = ({ data: post }) => {
   return (
-    <>
+    <div className=" group relative flex flex-col items-start">
       <div>
-        <h2>{post.title}</h2>
+        <h2 className="font-semibold">{post.title}</h2>
         <p>{post.content.text}</p>
       </div>
-    </>
+    </div>
   );
 };
 export default Post;
