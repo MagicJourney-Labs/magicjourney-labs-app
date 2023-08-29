fc4dd3fd8874ef0cfcbde3672ff46a5b175cfe9b
Martynas-J <Aurimhas@gmail.com>
2023-08-30 02:06:07 +0300

    fix isValid v2.0

--- src/app/api/revalidate/route.js
+++ src/app/api/revalidate/route.js
@@ -1,6 +1,8 @@
 import { processRequest } from "@/lib/processRequest";
 import { headers } from "next/headers";
 
+export const revalidate = true;
+
 export const POST = async (req) => {
   const secretKey = process.env.HYGRAPH_WEBHOOK_SECRET_REVALIDATE;
   const signatureHeader = headers(req).get("gcms-signature");
