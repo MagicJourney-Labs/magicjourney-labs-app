8be1eb35352be42290fb4da155b3ff57a7c0129b
Martynas-J <Aurimhas@gmail.com>
2023-08-29 09:18:29 +0300

    add Blog page v2,7

--- src/app/api/revalidateBlogs/route.js
+++ src/app/api/revalidateBlogs/route.js
@@ -1,51 +1,8 @@
-import { NextResponse } from "next/server";
-import { revalidateTag } from "next/cache";
-import { headers } from "next/headers";
-import { createHmac } from 'crypto';
+import { verifyAndProcessRequest } from "@/lib/verifyAndProcessRequest";
 
 export const revalidate = true;
 
-export const POST = async (req, res) => {
-  const tag = req.nextUrl.searchParams.get("tag");
-  if (!tag) {
-    return NextResponse.json({ message: "Missing tag param" }, { status: 400 });
-  }
-
-  const secret = process.env.HYGRAPH_BLOGS_WEBHOOK_KEY
-
-  const signature = headers().get("gcms-signature");
-
-  const [rawSign, rawEnv, rawTimestamp] = signature.split(', ');
-
-  const sign = rawSign.replace('sign=', '');
-  const EnvironmentName = rawEnv.replace('env=', '');
-  const Timestamp = parseInt(rawTimestamp.replace('t=', ''));
-
-  const bodyReq = await req.json()
-  const body = JSON.stringify(bodyReq)
-
-  let payload = JSON.stringify({
-      Body: body,
-      EnvironmentName,
-      TimeStamp: Timestamp,
-  });
-
-  const hash = createHmac('sha256', secret).update(payload).digest('base64');
-  const isValid = sign === hash;
-
-  if (req.method !== 'POST') {
-      return res.status(405).json({ message: 'Method Not Allowed' });
-  }
-
-  if (!isValid) {
-      return new NextResponse(" Invalid token Error :(", { status: 401 });
-  }
-
-  try {
-    revalidateTag(tag);
-
-    return new NextResponse({ revalidated: true }, { status: 200 });
-  } catch (err) {
-    return new NextResponse("Error while revalidating", { status: 500 });
-  }
+export const POST = async (req) => {
+  const secretKey = process.env.HYGRAPH_BLOGS_WEBHOOK_KEY;
+  return verifyAndProcessRequest(req, secretKey);
 };
--- src/lib/verifyAndProcessRequest.js
+++ src/lib/verifyAndProcessRequest.js
@@ -1,47 +1,47 @@
-// import { NextResponse } from "next/server";
-// import { revalidateTag } from "next/cache";
-// import { headers } from "next/headers";
-// import { createHmac } from 'crypto';
+import { NextResponse } from "next/server";
+import { revalidateTag } from "next/cache";
+import { headers } from "next/headers";
+import { createHmac } from 'crypto';
 
-// export const revalidate = true;
+export const revalidate = true;
 
-// export const verifyAndProcessRequest = async (req, secretKey) => {
-//   const tag = req.nextUrl.searchParams.get("tag");
-//   if (!tag) {
-//     return new NextResponse({ message: "Missing tag param" }, { status: 400 });
-//   }
+export const verifyAndProcessRequest = async (req, secretKey) => {
+  const tag = req.nextUrl.searchParams.get("tag");
+  if (!tag) {
+    return new NextResponse({ message: "Missing tag param" }, { status: 400 });
+  }
 
-//   const signature = headers().get("gcms-signature");
-//   const [rawSign, rawEnv, rawTimestamp] = signature.split(', ');
-//   const sign = rawSign.replace('sign=', '');
-//   const EnvironmentName = rawEnv.replace('env=', '');
-//   const Timestamp = parseInt(rawTimestamp.replace('t=', ''));
+  const signature = headers().get("gcms-signature");
+  const [rawSign, rawEnv, rawTimestamp] = signature.split(', ');
+  const sign = rawSign.replace('sign=', '');
+  const EnvironmentName = rawEnv.replace('env=', '');
+  const Timestamp = parseInt(rawTimestamp.replace('t=', ''));
 
-//   const bodyReq = await req.json()
-//   const body = JSON.stringify(bodyReq)
+  const bodyReq = await req.json()
+  const body = JSON.stringify(bodyReq)
 
-//   const payload = JSON.stringify({
-//       Body: body,
-//       EnvironmentName,
-//       TimeStamp: Timestamp,
-//   });
+  const payload = JSON.stringify({
+      Body: body,
+      EnvironmentName,
+      TimeStamp: Timestamp,
+  });
 
-//   const hash = createHmac('sha256', secretKey).update(payload).digest('base64');
-//   const isValid = sign === hash;
+  const hash = createHmac('sha256', secretKey).update(payload).digest('base64');
+  const isValid = sign === hash;
 
-//   if (req.method !== 'POST') {
-//       return new NextResponse({ message: 'Method Not Allowed' }, { status: 405 });
-//   }
+  if (req.method !== 'POST') {
+      return new NextResponse({ message: 'Method Not Allowed' }, { status: 405 });
+  }
 
-//   if (!isValid) {
-//       return new NextResponse(" Invalid token Error :(", { status: 401 });
-//   }
+  if (!isValid) {
+      return new NextResponse(" Invalid token Error :(", { status: 401 });
+  }
 
-//   try {
-//     revalidateTag(tag);
+  try {
+    revalidateTag(tag);
 
-//     return new NextResponse({ revalidated: true }, { status: 200 });
-//   } catch (err) {
-//     return new NextResponse("Error while revalidating", { status: 500 });
-//   }
-// };
+    return new NextResponse({ revalidated: true }, { status: 200 });
+  } catch (err) {
+    return new NextResponse("Error while revalidating", { status: 500 });
+  }
+};
