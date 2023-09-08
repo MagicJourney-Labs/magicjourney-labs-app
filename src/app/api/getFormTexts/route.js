f5dd069d467d9e886a2f19da3761c46451431c72
Martynas-J <Aurimhas@gmail.com>
2023-09-08 15:04:46 +0300

    MWPFE-35-Contacts-form-component-add-test-revalid

--- src/app/api/getFormTexts/route.js
+++ src/app/api/getFormTexts/route.js
@@ -2,6 +2,8 @@ import { fetchGraphQL } from '@/lib/graphqlUtils';
 import { contactsForm } from '@/queries/forms';
 import { NextResponse } from 'next/server';
 
+export const revalidate = true;
+
 export const GET = async () => {
   const data = await fetchGraphQL(contactsForm);
   return new NextResponse(JSON.stringify(data), {
