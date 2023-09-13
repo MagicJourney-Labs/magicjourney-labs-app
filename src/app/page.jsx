5847368cb307e075607813237ed3dc6cf0987356
Marius Ratnikas <marius.ratnikas@gmail.com>
2023-09-13 13:33:00 +0300

    fix home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,24 @@
 import { fetchGraphQL } from '@/lib/graphqlUtils';
-import OurProjects from '@/components/ui/OurProjects';
 import { home } from '@/queries/home';
+import { allServices } from '@/queries/services';
+import { allPosts } from '@/queries/posts';
 import HomePage from '@/components/ui/HomePage';
+import OurProjects from '@/components/ui/OurProjects';
+import Services from '@/components/ui/Services';
+import FeaturedPosts from '@/components/ui/Posts';
 
 export default async function Home() {
   const { homePage } = await fetchGraphQL(home);
+  const { aboutCard } = await fetchGraphQL(allServices);
+  const { posts } = await fetchGraphQL(allPosts, { next: { tags: ['posts'] } });
 
   return (
     <div className='flex  lg:w-full xl:flex  flex-col self-stretch'>
       <main className=' sm:px-8 md:px-8 lg:px-8 w-fit '>
         <HomePage data={homePage} />
         <OurProjects />
+        <Services data={aboutCard} />
+        <FeaturedPosts data={posts} />
       </main>
     </div>
   );
