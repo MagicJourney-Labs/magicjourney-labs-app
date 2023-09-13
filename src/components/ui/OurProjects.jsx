cbf7eb4d036b0b6cd99fc0cae1e5187710477c99
Marius Ratnikas <marius.ratnikas@gmail.com>
2023-09-13 15:01:03 +0300

    made changes

--- src/components/ui/OurProjects.jsx
+++ src/components/ui/OurProjects.jsx
@@ -1,12 +1,7 @@
 import OurProjectItem from './OurProjectItem';
-import { fetchGraphQL } from '@/lib/graphqlUtils';
-import { ourProjectItem } from '@/queries/ourProjectItem';
-import { ourProjects } from '@/queries/ourProjects';
 
-const OurProjects = async () => {
-  const { ourProjectsHeader } = await fetchGraphQL(ourProjects);
-  const { ourProjectItems } = await fetchGraphQL(ourProjectItem);
-  const { title, slagon } = ourProjectsHeader;
+const OurProjects = async ({ data }) => {
+  const { title, slagon } = data.ourProjectsHeader;
   return (
     <div className='mx-auto bg-white flex flex-col content-center gap-4 mt-20'>
       <div className='mx-auto h-10 text-center text-zinc-900 text-[2.62rem] font-bold leading-10 mt-4'>
@@ -16,7 +11,7 @@ const OurProjects = async () => {
         {slagon}
       </div>
       <div className='bg-gray-500 mt-16 mb-16 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-[1px]'>
-        {ourProjectItems.map((item) => {
+        {data.ourProjectItems.map((item) => {
           return <OurProjectItem key={item.id} data={item} />;
         })}
       </div>
