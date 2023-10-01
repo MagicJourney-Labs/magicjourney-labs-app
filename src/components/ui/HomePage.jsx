c9ca3a2004ed9219ca50d8c0f3f0a30f071df574
Martynas-J <Aurimhas@gmail.com>
2023-10-01 11:08:44 +0300

    MWPFE-48- fix home page buttons element style

--- src/components/ui/HomePage.jsx
+++ src/components/ui/HomePage.jsx
@@ -6,7 +6,8 @@ const HomePage = ({ data }) => {
   function paragraphStr(data) {
     const htmlStr = data.map((item) => {
       const originalHtml = item.html;
-      const modifiedHtml = originalHtml.slice(0, 2) + ' class="mt-6"' + originalHtml.slice(2);
+      const modifiedHtml =
+        originalHtml.slice(0, 2) + ' class="mt-6"' + originalHtml.slice(2);
 
       return modifiedHtml;
     });
@@ -25,19 +26,34 @@ const HomePage = ({ data }) => {
         <div className='mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-10 lg:mx-0 lg:max-w-none lg:grid-cols-2'>
           <div className='lg:pr-8 lg:pt-4'>
             <div className='lg:max-w-lg'>
-              <h1 className='mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>{data.title}</h1>
-              <div dangerouslySetInnerHTML={paragraphsElement} className='mt-6 text-lg leading-8 text-gray-600' />
-              <div className='flex xl:gap-10 md:gap-8 sm:gap-4 gap-2 items-center justify-center py-6'>
+              <h1 className='mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
+                {data.title}
+              </h1>
+              <div
+                dangerouslySetInnerHTML={paragraphsElement}
+                className='mt-6 text-lg leading-8 text-gray-600'
+              />
+              <div className='flex xl:gap-10 md:gap-8 sm:gap-4 gap-2 items-center justify-between py-6'>
                 {data.buttons.map((button) => (
-                  <Link key={button.id} href={`${button.page ? button.page.slug : '#'}`} className='text-sm font-semibold leading-6 text-gray-900'>
-                    <Button>{button.name}</Button>
+                  <Link
+                    key={button.id}
+                    href={`${button.page ? button.page.slug : '#'}`}
+                    className='text-sm font-semibold leading-6 text-gray-900 w-full'
+                  >
+                    <Button className='w-full'>{button.name}</Button>
                   </Link>
                 ))}
               </div>
             </div>
           </div>
           <div className='grid justify-self-center self-center'>
-            <Image src={`${data.imageUrl.url}`} alt={data.pictureAlt} className='w-[30rem] max-w-none rounded-xl ring-gray-400/10 sm:w-[30rem] md:-ml-4 lg:-ml-0' width={2200} height={1400} />
+            <Image
+              src={`${data.imageUrl.url}`}
+              alt={data.pictureAlt}
+              className='w-[30rem] max-w-none rounded-xl ring-gray-400/10 sm:w-[30rem] md:-ml-4 lg:-ml-0'
+              width={2200}
+              height={1400}
+            />
           </div>
         </div>
       </div>
