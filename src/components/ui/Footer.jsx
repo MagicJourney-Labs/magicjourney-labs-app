5313ca6af0d499281211b34f5ce56d0198b9bd87
Martynas-J <Aurimhas@gmail.com>
2023-10-01 11:41:00 +0300

    MWPFE-48- fix footer Copyright style

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -7,11 +7,17 @@ export default async function Footer({ data }) {
   const { footerInfo, copyright, socialMediaIcon, footerColumn } = data;
   return (
     <footer className='flex  xl:flex  flex-col  self-stretch items-center'>
-      <section className='xl:flex  pt-16 pb-12 flex-col  self-stretch border-t-2 border-t-gray-400'>
+      <section className='xl:flex  pt-16 pb-12 flex-col  self-stretch  shadow-[0px_0px_4px_2px_rgba(0,0,0,0.15)]'>
         <div className='flex-col mx-auto px-8 max-w-7xl justify-between sm:flex-col md:flex-col md:w-full md:gap-8 lg:flex-row xl:flex-row flex items-start lg:gap-16 gap-24 self-stretch'>
           <div className=' flex  flex-col items-start '>
             <div className='flex flex-row items-center '>
-              <Image src={`${footerInfo.image.url}`} alt='logo' width='60' height='60' /> <h5>{footerInfo.name}</h5>
+              <Image
+                src={`${footerInfo.image.url}`}
+                alt='logo'
+                width='60'
+                height='60'
+              />{' '}
+              <h5>{footerInfo.name}</h5>
             </div>
             <p className='text-gray-600'>{footerInfo.description}</p>
           </div>
@@ -22,15 +28,26 @@ export default async function Footer({ data }) {
           </div>
         </div>
       </section>
-      <section className='xl:flex  pt-12 pb-12 flex-col self-stretch border-t-2 border-t-gray-400'>
+      <section className='xl:flex  pt-6 pb-6 flex-col self-stretch '>
         <div className=' mx-auto flex-col max-w-7xl px-8 justify-between sm:flex-row md:flex-row md:w-full lg:flex-row xl:flex-row flex items-start lg:gap-16 gap-8 self-stretch'>
           <div>
-            <p className='text-gray-600'>{copyright.text}</p>
+            <p className='text-gray-600 text-sm'>{copyright.text}</p>
           </div>
           <div className='flex items-center gap-6'>
             {socialMediaIcon.map((icon) => (
-              <Link key={icon.url} href={`${icon.url}`} className='text-neutral-800 dark:text-neutral-200'>
-                {icon.image.url && <Image src={`${icon.image.url}`} width={30} height={30} alt='' />}
+              <Link
+                key={icon.url}
+                href={`${icon.url}`}
+                className='text-neutral-800 dark:text-neutral-200'
+              >
+                {icon.image.url && (
+                  <Image
+                    src={`${icon.image.url}`}
+                    width={25}
+                    height={25}
+                    alt=''
+                  />
+                )}
               </Link>
             ))}
           </div>
